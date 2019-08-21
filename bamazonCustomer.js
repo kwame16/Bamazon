776043a0579f7399a5c10dd5d0dfc761637065af
kwame16 <51427903+kwame16@users.noreply.github.com>
2019-08-20 23:15:44 -0400

    Finally found the error that was hidden

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 //installed mysq; & inquirer
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+require("console.table");
 
 // Created connection to my mySQL
 const connection = mysql.createConnection({
@@ -10,52 +11,66 @@ const connection = mysql.createConnection({
 
   user: "root",
 
-  password: "Lanyla2330!",
+  password: "password123",
   database: "bamazon"
 });
 
-// initallizing the mySQL connection 
+// initallizing the mySQL connection
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   displayProducts();
-
-  // Grabbing data from  table created in mySQL
-  function displayProducts() {
-    connection.query("SELECT * FROM products", function(err, data) {
-      if (err) throw err;
-      console.table(data);
-
-      buy();
-      //   function
-    });
-  }
 });
 
+// Grabbing data from  table created in mySQL
+function displayProducts() {
+  connection.query("SELECT * FROM products", function(err, data) {
+    if (err) throw err;
+    console.table(data);
+
+    buy();
+    //   function
+  });
+}
+
 function buy() {
-  inquirer.prompt([
-    {
-      name: "productSelect",
-      type: "input",
-      message: "Do you know the ID of the product you would like to purchase?"
-    },
-    {
+  inquirer
+    .prompt([
+      {
+        name: "productSelect",
+        type: "input",
+        message: "Do you know the ID of the product you would like to purchase?"
+      },
+      {
         name: "unitsWanted",
         type: "input",
         message: "How many units of the product would you like to buy?"
-    },
-  ])
-  .then(function(answer){
-    if (answer.post === "input"){
-      replinishInventory();
-    }
-    else{
-      connection.end();
-    }
-  })
+      }
+    ])
+    .then(function(answer) {
+      // console.log(answer);
 
-
+      if (answer) {
+        replinishInventory(answer.productSelect, answer.unitsWanted);
+        // console.log(answer);
+      } else {
+        connection.end();
+      }
+    });
 }
-// });
-
-//   connection.end();
+// created function to replinsh inventory taken from user
+function replinishInventory(product, quantity) {
+  let sentence ="UPDATE products SET stock_quantity = stock_quantity - ?  WHERE item_id = ? " 
+  console.log(sentence);
+  connection.query("UPDATE products SET stock_quantity = stock_quantity - ?  WHERE item_id = ? ",[quantity,product],
+    function(err, res) {
+      console.log(err);
+      console.log(res);
+      // console.log(res);
+      console.log(
+        "\nSuccessfully purchased " + quantity + " " + product + "'s!"
+      );
+      displayProducts();
+    }
+  );
+}
